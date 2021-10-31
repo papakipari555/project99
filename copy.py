061474d5e3ad24e0edc7458d8e0c76e5a6e002f3
papakipari555 <72609151+papakipari555@users.noreply.github.com>
2021-10-31 13:40:07 +0530

    Add files via upload

---
+++ copy.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+source= input("ENTER SOURCE FOLDER NAME: ")
+destination= input("ENTER DESTINATION FOLDER NAME: ")
+source=source+'/'
+destination= destination+'/'
+
+num=os.listdir(source)
+for file in num:
+    shutil.copy((source+file),destination)
